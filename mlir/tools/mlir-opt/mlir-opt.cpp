386cbadea2d62dcfd72f49d1b07952904ffe14f7
Paul Fuqua <pf@acm.org>
2021-09-02 19:04:52 -0500

    Move collapsing into a separate pass, and fix its competing versions.  Other little cleanups.

--- mlir/tools/mlir-opt/mlir-opt.cpp
+++ mlir/tools/mlir-opt/mlir-opt.cpp
@@ -107,9 +107,7 @@ void registerTestPreparationPassWithAllowedMemrefResults();
 void registerTestRecursiveTypesPass();
 void registerTestSCFUtilsPass();
 void registerTestVectorConversions();
-void registerTosaPartition1Pass();
-void registerTosaPartition2Pass();
-void registerTosaPartition3Pass();
+void registerTosaPartitionPass();
 } // namespace test
 } // namespace mlir
 
@@ -197,7 +195,7 @@ void registerTestPasses() {
   test::registerTestRecursiveTypesPass();
   test::registerTestSCFUtilsPass();
   test::registerTestVectorConversions();
-  test::registerTosaPartition1Pass();
+  test::registerTosaPartitionPass();
 }
 #endif
 
